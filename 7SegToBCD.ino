09e9c1deb87e06d1910a37cfdcd73a2a1d33a76e
Roberto <robertoca07@hotmail.com>
2020-07-20 16:29:34 -0500

    Primera versión funcional

--- 7SegToBCD.ino
+++ 7SegToBCD.ino
@@ -1,4 +1,7 @@
-//#include ".h"
+#include "Cabecera.h"
+
+unsigned char puerto;
+bool tipoDisplay = 1; // Catodo = 0, Anodo = 1
 
 // #define STRINGIFY_(M) #M   (Donvierte a M en string)
 
@@ -9,5 +12,26 @@ void setup() {
 }
 
 void loop() {
-  
+  puerto = ((PIND >> 1) | (PINB & (1<<PB0))); // Crear puerto (Leer estados 7 segmentos)
+
+  // TIPO DE DISPLAY
+  if(tipoDisplay) {
+    invertir(); 
+  }
+
+  // MATEMATICA BOOLEANA
+  bool A = ((c & !f) | (a & !e));                       // (C*~F)+(A*~E)
+  bool B = (!b & e) | (a & !f);                         // (~B*E)+(A*~F)
+  bool C = !b | (a & !d & !f) | (!a & f);               // (~B)+(A*~D*~F)+(~A*F)
+  bool D = a & b && f & g;                              // (A*B*F*G)
+
+  // IMPRIMIR RESULTADOS
+  digitalWrite(9, !A);
+  digitalWrite(10, !B);
+  digitalWrite(11, !C);
+  digitalWrite(12, !D);
+}
+
+void invertir() {
+  puerto = ~puerto;
 }
