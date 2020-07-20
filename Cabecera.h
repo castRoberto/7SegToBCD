ba2307daeeedf5082fc8ec958b986f8141788ef3
Roberto <robertoca07@hotmail.com>
2020-07-20 16:19:36 -0500

    Creación de las cabeceras

---
+++ Cabecera.h
@@ -0,0 +1,13 @@
+#ifndef CABECERA_H
+#define CABECERA_H
+#include <Arduino.h>
+
+#define a bool(puerto & (1<<1))
+#define b bool(puerto & (1<<2))
+#define c bool(puerto & (1<<3))
+#define d bool(puerto & (1<<4))
+#define e bool(puerto & (1<<5))
+#define f bool(puerto & (1<<6))
+#define g bool(puerto & (1<<0))
+
+#endif // CABECERA_H
