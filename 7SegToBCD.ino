f4e3e4ca262e7d0add95edb2769512e5e25c10fa
Roberto <robertoca07@hotmail.com>
2020-07-20 16:21:18 -0500

    Lectura de puertos por manipulación de registros

--- 7SegToBCD.ino
+++ 7SegToBCD.ino
@@ -3,7 +3,9 @@
 // #define STRINGIFY_(M) #M   (Donvierte a M en string)
 
 void setup() {
-  
+  // DIRECCIONAMIENTO DE PUERTOS
+  DDRD = 0b00000010;  // 0 = entrada, 1 = salida
+  DDRB = 0b00011110;
 }
 
 void loop() {
